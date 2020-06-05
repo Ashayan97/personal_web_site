b02634b3d2adb1054f749ee7c48113279cb4d5f5
Ashayan97 <shayan_stu@yahoo.com>
2020-06-06 01:38:14 +0430

    working on node js sever :(

--- ServerX1997.js
+++ ServerX1997.js
@@ -1,56 +1,66 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var async = require('async');
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
 http.createServer(
     function (req, res) {
-    var q = url.parse(req.url, true);
-    var address = q.pathname;
-    if (address == "/") {
-        // fs.readFile('HomePage.html', function (err, data) {
-        //     res.writeHead(200, {'Content-Type': 'text/html'});
-        //     res.write(data);
-        //     res.end();
-        // });
-        response.writeHead(200, {"Content-Type": "text/css"});
+        var header_type = "";
+        var data = "";
+        var q = url.parse(req.url, true);
+        var address = q.pathname;
+        if (address == "/") {
+            // fs.readFile('PageStyle.css', function (err, data2) {
+            //     res.writeHead(200, {'Content-Type': 'text/css'});
+            //     fs.readFile('HomePage.html', function (err, data) {
+            //         res.writeHead(200, {'Content-Type': 'text/html'});
+            //         res.write(data);
+            //         res.write(data2);
+            //         res.end();
+            //     });
+            // });
 
-        async.eachSeries(
-            ['css/bootstrap.css', 'css/bootstrap-responsive.css'],
-            function(filename, cb) {
-                fs.readFile(filename, function(err, content) {
-                    if (!err) {
-                        response.write(content);
-                    }
-                    cb(err);
+            var render = function (resource) {
+                // resource = name of resource (i.e. index, site.min, jquery.min)
+                fs.readFile(__dirname + "/" + resource, function (err, file) {
+                    if (err) return false; // Do something with the error....
+                    header_type = ""; // Do some checking to find out what header type you must send.
+                    data = file;
                 });
-            },
-            function(err) {
-                response.end()
-            }
-        );
-    } else {
-        if (address == "/blog") {
-            fs.readFile('BlogPage.html', function (err, data) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
+            };
+
+            get( function (req, res, next) {
+                // Send out the index.html
+                render('HomePage.html');
+                next();
             });
-        } else if (address == "/cv" || address == "/CV" || address == "/Cv" || address == "/cV") {
-            fs.readFile('CV.pdf', function (err, data) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
+
+
+            get( function (req, res, next) {
+                render('PageStyle.css');
+                next();
             });
         } else {
-            fs.readFile('404.html', function (err, data) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
-            });
+            if (address == "/blog") {
+                fs.readFile('BlogPage.html', function (err, data) {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(data);
+                    res.end();
+                });
+            } else if (address == "/cv" || address == "/CV" || address == "/Cv" || address == "/cV") {
+                fs.readFile('CV.pdf', function (err, data) {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(data);
+                    res.end();
+                });
+            } else {
+                fs.readFile('404.html', function (err, data) {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(data);
+                    res.end();
+                });
+            }
         }
-    }
 
-}).listen(8080);
+    }).listen(8080);
