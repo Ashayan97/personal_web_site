f98fc74c07ba4ae6ed53953e671b1fb559100202
Ashayan97 <shayan_stu@yahoo.com>
2020-06-06 01:19:44 +0430

    design finished totaly now just nod.js server left :))

--- ServerX1997.js
+++ ServerX1997.js
@@ -1,18 +1,36 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-app.use(express.static(path.join(__dirname, 'public')));
+var async = require('async');
+
+// app.use(express.static(path.join(__dirname, 'public')));
 
 http.createServer(
     function (req, res) {
     var q = url.parse(req.url, true);
     var address = q.pathname;
     if (address == "/") {
-        fs.readFile('HomePage.html', function (err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        });
+        // fs.readFile('HomePage.html', function (err, data) {
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.write(data);
+        //     res.end();
+        // });
+        response.writeHead(200, {"Content-Type": "text/css"});
+
+        async.eachSeries(
+            ['css/bootstrap.css', 'css/bootstrap-responsive.css'],
+            function(filename, cb) {
+                fs.readFile(filename, function(err, content) {
+                    if (!err) {
+                        response.write(content);
+                    }
+                    cb(err);
+                });
+            },
+            function(err) {
+                response.end()
+            }
+        );
     } else {
         if (address == "/blog") {
             fs.readFile('BlogPage.html', function (err, data) {
